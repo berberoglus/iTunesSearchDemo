73257b11ab7811aa303a710b70e5520ce422586b
Samet Berberoglu <SametBerberoglu01@gmail.com>
2025-04-13 23:53:50 +0200

    Create Xcode project

---
+++ ITunesSearch/ITunesSearch/ITunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesSearchApp.swift
+//  ITunesSearch
+//
+//  Created by Samet Berberoğlu on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct ITunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
